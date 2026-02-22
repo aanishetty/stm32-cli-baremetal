d24f9e971e474faa7fe71f6f1306a37a227c4fbb
Agastya Anishetty <aggy@Agastyas-MacBook-Air.local>
2026-02-22 10:38:54 -0800

    cli: fix backspace/delete handling

--- Src/cli.c
+++ Src/cli.c
@@ -20,6 +20,8 @@ void cli_task(cli_interface_t *cli)
 	if (ch == '\n')
 	{
 		cli->input_buffer[cli->buffer_index] = '\0';
+		cli->write_char('\r');
+		cli->write_char('\n');
         cli_process_line(cli, cli->input_buffer);
 		cli->buffer_index = 0;
 		return;
@@ -28,6 +30,17 @@ void cli_task(cli_interface_t *cli)
 	// If it's null character or carriage return ('/r'), then return
 	if (ch == 0xFF || ch == '\r')
 	{
+		return;
+	}
+
+	// If it's backspace or delete move index back one, erase on terminal, space and backspace
+	if (ch == 0x08 || ch == 0x7F)
+	{
+		cli->buffer_index = cli->buffer_index-1;
+		cli->input_buffer[cli->buffer_index] = '\0';
+		cli->write_char('\b');
+		cli->write_char(' ');
+		cli->write_char('\b');
 		return;
 	}
 
@@ -40,6 +53,7 @@ void cli_task(cli_interface_t *cli)
 	// Add character to the buffer and update index
 	cli->input_buffer[cli->buffer_index] = (char)ch;
 	cli->buffer_index++;
+	cli->write_char((char)ch);
 }
 
 void cli_process_line (cli_interface_t *cli, const char* input)
